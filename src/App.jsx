9036778b7bc918d10e95d84ea0f4f90fb141cdd7
SupremeRubisco <92188403+SupremeRubisco@users.noreply.github.com>
2026-03-19 13:50:09 -0400

    Add Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,75 @@
-import { useState } from 'react'
-import Navbar from './Navbar.jsx'
+import { useState } from "react";
+import Navbar from "./Navbar.jsx";
 
 function App() {
+    return (
+        <>
+            <head>
+                <title>Scoopermania</title>
+            </head>
 
-  return (
-      <>
-      <head>
-        <title>Scoopermania</title>
-      </head>
+            <body>
+                <div id="screen-dimmer" onClick="exitInfo()"></div>
 
-      <body>
+                <Navbar />
 
-      <div id="screen-dimmer" onClick="exitInfo()"></div>
+                <div id="center" className="glass">
+                    <div id="countdown-container">
+                        <div className="time-unit">
+                            <span className="number" id="days">
+                                00
+                            </span>
+                            <span className="label">Days</span>
+                        </div>
+                        <span className="colon">:</span>
+                        <div className="time-unit">
+                            <span className="number" id="hours">
+                                00
+                            </span>
+                            <span className="label">Hours</span>
+                        </div>
+                        <span className="colon">:</span>
+                        <div className="time-unit">
+                            <span className="number" id="minutes">
+                                00
+                            </span>
+                            <span className="label">Minutes</span>
+                        </div>
+                        <span className="colon">:</span>
+                        <div className="time-unit">
+                            <span className="number" id="seconds">
+                                00
+                            </span>
+                            <span className="label">Seconds</span>
+                        </div>
+                    </div>
+                </div>
 
-      <Navbar/>
+                <div id="scrollBtn">
+                    <button className="mainBtn" id="scroll" onClick="mainBtnClick()">
+                        More Info
+                    </button>
+                </div>
 
-      <div id="center" className="glass">
-        <div id="countdown-container">
-          <div className="time-unit">
-            <span className="number" id="days">00</span>
-            <span className="label">Days</span>
-          </div>
-          <span className="colon">:</span>
-          <div className="time-unit">
-            <span className="number" id="hours">00</span>
-            <span className="label">Hours</span>
-          </div>
-          <span className="colon">:</span>
-          <div className="time-unit">
-            <span className="number" id="minutes">00</span>
-            <span className="label">Minutes</span>
-          </div>
-          <span className="colon">:</span>
-          <div className="time-unit">
-            <span className="number" id="seconds">00</span>
-            <span className="label">Seconds</span>
-          </div>
-        </div>
-      </div>
-
-      <div id="scrollBtn">
-        <button className="mainBtn" id="scroll" onClick="mainBtnClick()">More Info</button>
-      </div>
-
-      <div className="infoRight glass">
-        <p id="exit" onClick="exitInfo()">X</p>
-        <h2 id="infoTitle">More Info</h2>
-        <h3 className="larger">What even is Scoopermania?</h3>
-        <p className="info">Scoopermania is an annual all-you-can-eat ice cream fundraiser for the Jimmy Fund and
-          Dana-Farber Cancer Institute!</p>
-        <h3 className="larger">When is it?</h3>
-        <p className="info">Scoopermania will be held on Saturday, May 17, 2025, from 11:00 AM to 4:00 PM at the <a
-            id="map" href="https://maps.apple.com/directions?destination=42.415726%2C+-71.15239&mode=driving">Cyrus
-          Dallin Art Museum.</a></p>
-      </div>
-
-      </body>
-      <script src="script.js"></script>
-      </>
-  )
+                <div className="infoRight glass">
+                    <p id="exit" onClick="exitInfo()">
+                        X
+                    </p>
+                    <h2 id="infoTitle">More Info</h2>
+                    <h3 className="larger">What even is Scoopermania?</h3>
+                    <p className="info">Scoopermania is an annual all-you-can-eat ice cream fundraiser for the Jimmy Fund and Dana-Farber Cancer Institute!</p>
+                    <h3 className="larger">When is it?</h3>
+                    <p className="info">
+                        Scoopermania will be held on Saturday, May 17, 2025, from 11:00 AM to 4:00 PM at the{" "}
+                        <a id="map" href="https://maps.apple.com/directions?destination=42.415726%2C+-71.15239&mode=driving">
+                            Cyrus Dallin Art Museum.
+                        </a>
+                    </p>
+                </div>
+            </body>
+            <script src="script.js"></script>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,29 +1,31 @@
-import { barHover, barOff, itemHover, itemLeave } from './script.js'; // You'll need to export these from script.js
-import {Link} from 'react-router';
+import { barHover, barOff, itemHover, itemLeave } from "./script.js"; // You'll need to export these from script.js
+import { Link } from "react-router";
 
 export default function Navbar() {
     return (
-
         <div id="bar" onMouseEnter={barHover} onMouseLeave={barOff} className="glass">
-
             <div id="container">
-
                 <div id="home" className="overlap" onMouseEnter={(e) => itemHover(e.currentTarget)} onMouseLeave={(e) => itemLeave(e.currentTarget)}>
-                    <img src="/home.png" alt="Home" className="image" id="homeIcn"/>
-                    <button id="homeBtn" className="btn"><Link to={'/'}>Home</Link></button>
+                    <img src="/home.png" alt="Home" className="image" id="homeIcn" />
+                    <button id="homeBtn" className="btn">
+                        <Link to={"/"}>Home</Link>
+                    </button>
                 </div>
 
                 <div id="iceCream" className="overlap" onMouseEnter={(e) => itemHover(e.currentTarget)} onMouseLeave={(e) => itemLeave(e.currentTarget)}>
-                    <img src="/ticket.png" alt="Ice Cream" className="image" id="iceCreamIcn"/>
-                    <button id="iceCreamBtn" className="btn"><Link to="/tickets">Tickets</Link></button>
+                    <img src="/ticket.png" alt="Ice Cream" className="image" id="iceCreamIcn" />
+                    <button id="iceCreamBtn" className="btn">
+                        <Link to="/tickets">Tickets</Link>
+                    </button>
                 </div>
 
                 <div id="about" className="overlap" onMouseEnter={(e) => itemHover(e.currentTarget)} onMouseLeave={(e) => itemLeave(e.currentTarget)}>
-                    <img src="/heart.png" alt="supporters" className="image" id="donateIcn"/>
-                    <button id="aboutBtn" className="btn"><Link to="/supporters">Patrons</Link></button>
+                    <img src="/heart.png" alt="supporters" className="image" id="donateIcn" />
+                    <button id="aboutBtn" className="btn">
+                        <Link to="/supporters">Patrons</Link>
+                    </button>
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
