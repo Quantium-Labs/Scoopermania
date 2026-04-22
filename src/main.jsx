687c8d3b86875efd183073b3ed31e041156c6d4a
SupremeRubisco <92188403+SupremeRubisco@users.noreply.github.com>
2026-04-22 19:35:40 -0400

    Quick fix for now

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Supporters from './Pages/Supporters.jsx'
 import Tickets from './Pages/Tickets.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/Scoopermania">
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/supporters" element={<Supporters />} />
